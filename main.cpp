43139bf14617c0ec3c22a4980e40229d7065673e
GoobyMain <gavindw6@gmail.com>
2024-11-18 15:01:08 -0800

    changed main

--- main.cpp
+++ main.cpp
@@ -7,9 +7,14 @@ int main() {
 
     BinaryMaxHeap<int> heap1;
     BinaryMaxHeap<std::string> heap2;
+    BinaryMaxHeap<int> heap3;
 
     heap1.insert(3, 4);
-    heap2.insert(3, "four");
+    heap2.insert(8, "four");
+    heap3.insert(5, 90);
+    heap1.insert(2, 9);
+
+    BinaryMaxHeap<int> heap4 = BinaryMaxHeap::merge(heap1, heap2);
 
     return 0;
 
